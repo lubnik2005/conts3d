8999faeca3b94384e263c4dfd0592899f2466ec9
lubnik2005 <lubnik2005_@gmail.com>
2024-08-06 19:58:03 -0700

    Added conts3d.f

---
+++ conts3d.f
@@ -0,0 +1,13 @@
+      program pin
+
+c get input from user
+         call setup
+
+c read contour data
+         call rconts
+
+c generate 3d plot of contours
+         call dr3d
+
+         stop
+      end
